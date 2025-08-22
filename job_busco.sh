386781f2daf899aefcc0b08d786263927e0ca636
Tiago Belintani <113792806+TiagoBelintani@users.noreply.github.com>
2025-08-22 12:26:23 -0700

    Update job_busco.sh

--- job_busco.sh
+++ job_busco.sh
@@ -9,7 +9,7 @@ module load miniconda/3-2023-09
 source activate /home/tiagobelintani/miniconda3/envs/stats_assembly # Troque pelo seu caminho
 
 # Executando o BUSCO com estilo e respeito ao cluster
-busco -i /home/tiagobelintani/Matheus/transcriptomas/assembly/trinity_assembly_SRR1514881.Trinity.fasta\
+busco -i /home/tiagobelintani/Matheus/transcriptomas/assembly/trinity_assembly_SRR1514881.Trinity.fasta \
       -l /home/tiagobelintani/transcriptoma/busco_analises/araneae_odb12 \
       -o /home/tiagobelintani/transcriptoma/busco_analises/SRR1514881_BUSCO \
       -m transcriptome \
